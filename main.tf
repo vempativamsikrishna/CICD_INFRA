cf0c6781759919c35164e4890fe5ce0aa3cd09c4
vempativamsikrishna <132368115+vempativamsikrishna@users.noreply.github.com>
2023-08-23 17:51:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,11 +100,11 @@ resource "aws_security_group" "allow_all" {
     }
 }
 
-data "aws_ami" "my_ami" {
-     most_recent      = true
-     #name_regex       = "^mavrick"
-     owners           = ["721834156908"]
-}
+#data "aws_ami" "my_ami" {
+#     most_recent      = true
+#     #name_regex       = "^mavrick"
+#     owners           = ["721834156908"]
+#}
 
 resource "aws_instance" "Public-instances" {
     count = 1
